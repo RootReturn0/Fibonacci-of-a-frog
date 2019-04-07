99812104ec96c0361982bcd54035092638d89226
RootReturn0 <34030602+RootReturn0@users.noreply.github.com>
2019-04-07 17:29:59 +0800

    Update answer.cpp

--- answer.cpp
+++ answer.cpp
@@ -4,10 +4,12 @@ Author:Feifan Wang
 
 Date:04-07-2019
 
+Email: feifan_wang@qq.com
+
 Description: The answer of how many methods the frog
              can use when it jumps to n steps, which
-             can jump up one or two steps each time.            
-             
+             can jump up one or two steps each time.   
+
 **************************************************/
 
 #include <iostream>
@@ -17,8 +19,8 @@ using namespace std;
 
 /**
  * @brief 2 * 2 matrix 
- * ⌈x1 x2⌉
- * ⌊y1 y2⌋
+ * @deteil ⌈x1 x2⌉
+ *         ⌊y1 y2⌋
  */
 struct Matrix
 {
@@ -27,13 +29,13 @@ struct Matrix
     unsigned long long y1;
     unsigned long long y2;
 
-    Matrix(){}
-    Matrix(unsigned long long a,unsigned long long b,unsigned long long c, unsigned long long d)
+    Matrix() {}
+    Matrix(unsigned long long a, unsigned long long b, unsigned long long c, unsigned long long d)
     {
-        x1=a;
-        x2=b;
-        y1=c;
-        y2=d;
+        x1 = a;
+        x2 = b;
+        y1 = c;
+        y2 = d;
     }
 };
 
@@ -41,6 +43,8 @@ struct Matrix
  * @brief matrix multiplication
  * @param a @see Matrix
  * @param b @see Matrix
+ * 
+ * @return re @see Matrix
  */
 Matrix mul(const Matrix &a, const Matrix &b)
 {
@@ -55,33 +59,38 @@ Matrix mul(const Matrix &a, const Matrix &b)
 
 /** 
  * @brief matrix power
+ * @example
+ *  x^5=x*(x^4) 5=bin(101)
+ *  x^9=x*(x^8) 9=bin(1001)
  * @param a @see Matrix
  * @param n //power
+ * 
+ * @return re @see Matrix
  */
 Matrix pow(Matrix a, int n)
 {
-    Matrix re(1,1,1,1);
-    while(n)
+    n -= 1; //n power operates (n - 1) times
+    Matrix re = a;
+    while (n)
     {
-        if(n&1)
-            re=mul(re,a);//re=re*a
-        a=mul(a,a);//a=a*a
-        n>>=1;
+        if (n & 1)
+            re = mul(re, a); //re=re*a
+        a = mul(a, a);       //a=a*a
+        n >>= 1;
     }
 
     return re;
 }
 
 /**
- * Fibonacci number
- * @brief Find the Fibonacci number of some index
+ * @brief find the Fibonacci number of some index
  * @param index (index>=2)
  */
 void Fibonacci(int index)
 {
     Matrix init(1,1,1,0);
-    Matrix fin=pow(init, index-1);//index starts from 0 in Fibonacci sequence
-    cout<<fin.x1<<endl;
+    Matrix re=pow(init, index);//the index starts from 0 in Fibonacci sequence
+    cout<<re.x1<<endl;
 }
 
 int main()
@@ -89,6 +98,12 @@ int main()
     int n;
     cout<<"Please enter the number of steps the frog need to jump up: ";
     cin >> n;
+    while(n<0)//n must be natural number
+    {
+        cout<<"Entered number is illegal! The number must be non-negative number!"<<endl;
+        cout<<"Please enter the number of steps the frog need to jump up: ";
+        cin >> n;
+    }
     cout<<"The number of methods the frog can use is: ";
     if (n ==0)//the frog can not jump up steps if there are no steps
         cout << n << endl;
@@ -96,6 +111,7 @@ int main()
     {
         Fibonacci(n);
     }
-
+    
     return 0;
 }
+
